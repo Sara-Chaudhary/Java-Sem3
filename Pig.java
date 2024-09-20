1dbfb8f7223846c804cf2831296008038b259fca
Sara-Chaudhary <sara.siwaya@gmail.com>
2024-09-20 10:37:05 +0530

    add

--- Pig.java
+++ Pig.java
@@ -6,17 +6,16 @@ interface Animal {
 
 // Pig "implements" the Animal interface
 class Pig implements Animal {
+  @Override
   public void animalSound() {
     // The body of animalSound() is provided here
     System.out.println("The pig says: wee wee");
   }
+  @Override
   public void run() {
     // The body of run() is provided here
     System.out.println("Pig is running");
   }
-}
-
-class Main {
   public static void main(String[] args) {
     Pig myPig = new Pig();  // Create a Pig object
     myPig.animalSound();
